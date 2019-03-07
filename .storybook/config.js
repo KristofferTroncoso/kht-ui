d38d08facabc9940badf5ed14327e39c83e5f816
Kristoffer <troncosokristoffer@gmail.com>
2019-03-07 00:02:37 -0500

    Add backgrounds addon functionality

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,24 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+
+addParameters({
+  options: {
+    name: 'KHT UI Storybook',
+    url: 'https://github.com/KristofferTroncoso/kht-ui',
+    //goFullScreen: false,
+    showAddonsPanel: true,
+    showSearchBox: true,
+    addonPanelInBottom: true,
+    //sortStoriesByKind: false,
+    //hierarchySeparator: /\./,
+    //hierarchyRootSeparator: /\|/,
+    //enableShortcuts: true,
+  },
+  backgrounds: [
+    { name: 'dark', value: '#2d2d2d'},
+    { name: 'KT', value: '#fff4ea'},
+    { name: 'tomato', value: 'tomato'},
+  ]
+});
 
 function loadStories() {
   require('../stories/index.js');
