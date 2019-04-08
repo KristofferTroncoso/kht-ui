c2b047fcb58bd9446bc7f0c9c331d889d6f40b50
Kristoffer <troncosokristoffer@gmail.com>
2019-04-08 17:49:31 -0400

    Add some snapshot tests

--- src/components/Code/__tests__/Code.test.js
+++ src/components/Code/__tests__/Code.test.js
@@ -2,7 +2,17 @@ import React from 'react';
 import {Code} from '../Code';
 import Renderer from 'react-test-renderer';
 
-test('default', () => {
+test('normal render', () => {
   const wrapper = Renderer.create(<Code render={`let i = 3`} />);
-  expect(wrapper);
-})
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('inline render', () => {
+  const wrapper = Renderer.create(<Code inline render={`let i = 3`} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('dark theme', () => {
+  const wrapper = Renderer.create(<Code theme="dark" render={`let i = 3`} />);
+  expect(wrapper).toMatchSnapshot();
+});
