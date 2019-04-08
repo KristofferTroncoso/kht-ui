48e7235e381a71d4fa9d023ae13bfd1d2286994c
Kristoffer <troncosokristoffer@gmail.com>
2019-04-08 05:02:17 -0400

    Add test to Code component

---
+++ src/components/Code/__tests__/Code.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Code} from '../Code';
+import Renderer from 'react-test-renderer';
+
+test('default', () => {
+  const wrapper = Renderer.create(<Code render={`let i =3`} />);
+  expect(wrapper);
+})
