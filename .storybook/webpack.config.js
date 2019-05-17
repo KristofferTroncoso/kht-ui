f2da385a8779ccac3879f8836d88c381cf4970f0
Kristoffer <troncosokristoffer@gmail.com>
2019-05-17 17:32:13 -0400

    Add storysource addon for storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,13 @@
-const path = require('path');
 module.exports = ({ config, mode }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
       {
         loader: require.resolve('awesome-typescript-loader'),
+      },
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: { parser: 'typescript' },
       },
       // Optional
       // {
